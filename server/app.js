19ee07ad5beee66377976cb882124f3909d363b7
ThingKi <dewyday@naver.com>
2018-02-28 11:52:43 +0900

    express init

--- server/app.js
+++ server/app.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const app = express();
 
-app.use('/', (req,res,next)=>{
-    console.log(req.url);
-    if(req.url.indexOf(".html")!=-1){
-        res.sendFile(req.url,{root:__dirname});
-    }
-    res.sendFile("/index.html",{root:__dirname});
-})
+var initFunc = function(req,res,next){
+    console.log("req=>", req);
+    console.log("res=>", res);
+    res.send("hello~");
+    next();
+}
+app.post('/',initFunc);
+app.get('/',initFunc);
+// app.use('/', (req,res,next)=>{
+//     console.log(req.url);
+//     if(req.url.indexOf(".html")!=-1){
+//         res.sendFile(req.url,{root:__dirname});
+//     }
+//     res.sendFile("/index.html",{root:__dirname});
+// })
 app.listen(4003,function(){
     console.log("Express start : port[4003]");
 })
